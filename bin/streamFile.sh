ee12829866399e7be47df8249e54214f412b1f29
Matthew Campbell <matt@littlelionstudios.com>
2017-03-01 17:06:19 -0800

    Added support to create an arbitrary number of test streams and specify a file to stream and a set of ports to stream from.

--- bin/streamFile.sh
+++ bin/streamFile.sh
@@ -1,4 +1,39 @@
 #!/bin/bash
 
-vlc 20150111_150020.mp4 --sout '#http{mux=ffmpeg{mux=flv},dst=:8080/}'
+# Start up a specified number of test streams on a sequential series of ports
+
+# Usage: $0 [streams] [videofile] [firstport]
+
+# Danger: does a killall vlc so will muck with other running VLC processes
+
+PLAYER='/usr/bin/vlc'
+DEBUG=
+# DEBUG=echo
+
+COUNT=$1
+if [ -z "$COUNT" ] ; then
+    COUNT=1
+fi
+
+FILE=$2
+if [ -z "$FILE" ] ; then
+    FILE="$HOME/20150111_150020.mp4"
+fi
+
+PORT=$3
+if [ -z "$PORT" ] ; then
+    PORT=8080
+fi
+
+# Shut down any running players
+killall $PLAYER
+
+# Create the requested number of streams
+NUM=1
+while [ $NUM -le $COUNT ] ; do
+    $DEBUG $PLAYER $FILE --sout "#http{mux=ffmpeg{mux=flv},dst=:$PORT/}" &
+
+    PORT=`expr $PORT + 1`
+    NUM=`expr $NUM + 1`
+done
 
