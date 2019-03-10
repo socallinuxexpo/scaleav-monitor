da096ff30c1e10b374dae0d4550c17901b148ff8
M Starch <lestarch@googlemail.com>
2019-03-10 16:03:51 -0700

    Added debugging output.  Added parsing of the new rooms file format.  Added greying out entries with a status of off.

--- bin/monitor_ip.pl
+++ bin/monitor_ip.pl
@@ -8,6 +8,7 @@
 # FUTURE features
 # - Check application level response
 #   - http status query and response analysis
+# - Rescan room file every once in a while and reconfigure if it changes
 
 # Pragmas
 use strict;
@@ -30,6 +31,8 @@ my $scan_interval   = 5.00;        # Rescan rate in seconds
 my $check_network   = 1;           # Perform a ping test
 my $check_app       = 0;           # Perform a protocol status query
 my $play_sounds     = 0;
+my $debug           = 4;
+
 # Define column titles and associated hostname rules
 #   append the suffix to the base hostname to create the associated hardware FQDN
 my $machines = [
@@ -43,15 +46,8 @@ my $machines = [
 # Derived constants
 my $ping = Net::Ping->new();
 my $rooms = readFile($datafile);
-my $names = [];
 my $status = {};
-foreach my $line (@$rooms) {
-    (my $name = $line) =~ s/.*http:\/\///;
-    $name =~ s/:8080.*//;
-    next if ($name =~ m/^extra/);
-    $status->{$name} = {};
-    push @$names, $name;
-}
+my $names = parseFile($rooms);
 
 parseArgs(@ARGV);
 main();
@@ -65,32 +61,42 @@ sub main {
         printf("%-25.25s %-8s %-8s %-8s\n", 'Room', map $_->[0], @$machines);
         my $in_danger = 0;
         foreach my $room (sort @$names) {
+            print (($status->{$room}->{'state'} eq 'off')
+                ? dkgrey_fg()
+                : white_fg());
             printf('%-25.25s', "$room: ");
             foreach my $machine (@$machines) {
                 (my $hostname = $room) =~ s/\./$machine->[1]./;
-		if ($check_app) {
+                if ($check_app) {
                     # For each line determine an application level query and
-                    # set of responses.  Use wget or library to send the query
-                    # and receive back responses.  If fails check network below
-		} elsif ($check_network) {
+                    # set of responses.  Use wget or library to send the
+                    # query and receive back responses.  If fails check
+                    # the network below
+                } elsif ($check_network) {
+		    print low_intensity()
+                        if ($status->{$room}->{'state'} eq 'off');
                     if ($ping->ping($hostname, $ping_timeout)) {
                         $status->{$room}->{$machine} = 0;
-                        print green_dot();
+			print green_fg(), green_dot(), white_fg();
                     } else {
                         $status->{$room}->{$machine}++;
                         if ($status->{$room}->{$machine} >= 2 and 
                                 $status->{$room}->{$machine} <= 5) {
-                            print red_bg(), red_dot(), black_bg();
+                            if ($status->{$room}->{'state'} eq 'off') {
+                                print red_fg(), red_dot(), white_fg();
+                            } else {
+                                print red_bg(), black_fg(), red_dot(), black_bg(), white_fg();
+                            }
                             $in_danger = 1;
                         } else {
-                            print red_dot();
+                            print red_fg(), red_dot(), white_fg();
                         }
                     }
                 }
             }
             print "\n";
         }
-	play_sound(($sound_toggle++ % 2) ? $soundfile2 : $soundfile)
+        play_sound(($sound_toggle++ % 2) ? $soundfile2 : $soundfile)
             if ($play_sounds and $in_danger);
         sleep $scan_interval;
     }
@@ -153,23 +159,23 @@ EOF
 }
 
 sub red_dot {
-    return red_fg() . ' R       ' . white_fg();
+    return ' R       ';
 }
 
 sub green_dot {
-    return green_fg() . ' G       ' . white_fg();
+    return ' G       ';
 }
 
 sub red_fg {
-    return "\033[31;1m";
+    return "\033[31m";
 }
 
 sub red_bg {
-    return "\033[41;1m";
+    return "\033[41m";
 }
 
 sub green_fg {
-    return "\033[32;1m";
+    return "\033[32m";
 }
 
 sub white_fg {
@@ -180,8 +186,20 @@ sub white_bg {
     return "\033[47;1m";
 }
 
+sub black_fg {
+    return "\033[30;1m";
+}
+
 sub black_bg {
-    return "\033[40;1m";
+    return "\033[40;0m";
+}
+
+sub dkgrey_fg {
+    return "\033[30;1m";
+}
+
+sub low_intensity {
+    return "\033[0m";
 }
 
 sub home_screen {
@@ -195,6 +213,7 @@ sub clear_screen {
 sub readFile {
     my ($file) = @_;
 
+    print "Reading '$file'\n" if ($debug >= 2); 
     open FD, "<$file" or die "Error opening file '$file': $!\ n";
     my @output = <FD>;
     close FD;
@@ -203,9 +222,28 @@ sub readFile {
     return \@output;
 }
 
+sub parseFile {
+    my $names = [];
+
+    print "Reading data\n" if ($debug >= 2); 
+    foreach my $line (@$rooms) {
+        print "  line: $line\n" if ($debug >= 8);
+        $line =~ s/[!;#].*//;
+        next if ($line =~ m/^\s*$/);
+        my ($state, $fqdn) = ($line =~ m/\s*(\S*).*http:\/\/(.*):8080.*/);
+        print "    state: $state, name: $fqdn\n" if ($debug >= 6);
+        $status->{$fqdn} = {};
+        $status->{$fqdn}->{'state'} = $state;
+        push @$names, $fqdn;
+    }
+
+    return $names;
+}
+
 sub play_sound {
     my ($sound) = @_;
 
+    print "Playing $sound\n" if ($debug >= 8);
     system("cvlc --play-and-exit $sound_dir/$sound > /dev/null 2>/dev/null");
 }
 
