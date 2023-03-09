a15b772a029a4abc9f065b2d15e7db15902b0ba4
M Starch <LeStarch@googlemail.com>
2023-03-08 17:48:26 -0800

    Saved default room configuration for the Pasadena Convention Center and the Airport Hilton Hotel.

---
+++ config/rooms.pas
@@ -0,0 +1,15 @@
+off     rtmp://ballroom-a.scaleav.us:1935/scale/mixed
+off     rtmp://ballroom-b.scaleav.us:1935/scale/mixed
+off     rtmp://ballroom-c.scaleav.us:1935/scale/mixed
+off     rtmp://ballroom-de.scaleav.us:1935/scale/mixed
+off     rtmp://ballroom-f.scaleav.us:1935/scale/mixed
+off     rtmp://ballroom-g.scaleav.us:1935/scale/mixed
+off     rtmp://ballroom-h.scaleav.us:1935/scale/mixed
+
+off     rtmp://room-101.scaleav.us:1935/scale/mixed
+off     rtmp://room-103.scaleav.us:1935/scale/mixed
+off     rtmp://room-105.scaleav.us:1935/scale/mixed
+off     rtmp://room-106.scaleav.us:1935/scale/mixed
+off     rtmp://room-107.scaleav.us:1935/scale/mixed
+off     rtmp://room-211.scaleav.us:1935/scale/mixed
+
